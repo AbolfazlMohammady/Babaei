1823101f7c690ff0010d2fb624cadabdbd4cc51e
abolfazll mohammadi <156206718+AbolfazlMohammady@users.noreply.github.com>
2026-09-16 10:41:28 +0330

    feat(shop): mount shop urls

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,26 +1,25 @@
-from django.urls import path, include
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import JsonResponse
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.contrib import admin
+from django.urls import include, path
 
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 
 urlpatterns = [
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),  # فایل JSON
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    path('admin/', admin.site.urls),
-
-
-    # My app
-    
+    path("admin/", admin.site.urls),
     path("", include("apps.home.urls")),
-    path('', include('apps.users.urls')),
-
+    path("", include("apps.users.urls")),
+    path("", include("apps.shop.urls")),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
