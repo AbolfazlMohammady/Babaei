b2b78acdb883f3f5498b5e2b2b44c3dc34dbba95
abolfazll mohammadi <156206718+AbolfazlMohammady@users.noreply.github.com>
2026-09-16 11:15:09 +0330

    fix(seo): keep shop URL module focused on storefront routes

--- config/apps/shop/urls.py
+++ config/apps/shop/urls.py
@@ -1,34 +1,9 @@
-from django.conf import settings
-from django.contrib.sitemaps.views import sitemap
-from django.http import HttpResponse
 from django.urls import path
-from django.views.decorators.cache import cache_page
-from django.views.decorators.http import require_GET
 
-from .sitemaps import sitemaps
 from .views import CategoryDetailView, ProductDetailView, ShopIndexView
 
 
 app_name = "shop"
-
-
-@require_GET
-def robots_txt(request):
-    content = "\n".join(
-        [
-            "User-agent: *",
-            "Allow: /",
-            "Disallow: /admin/",
-            "Disallow: /api/",
-            "Disallow: /login/",
-            "Disallow: /register/",
-            "Disallow: /logout/",
-            "",
-            f"Sitemap: {settings.SITE_URL}/sitemap.xml",
-        ]
-    )
-    return HttpResponse(content, content_type="text/plain; charset=utf-8")
-
 
 urlpatterns = [
     path("", ShopIndexView.as_view(), name="index"),
