8969366250cf5b56b1621efffccc97e2b579289f
abolfazll mohammadi <156206718+AbolfazlMohammady@users.noreply.github.com>
2026-09-19 09:53:23 +0330

    Remove unused favorite import

--- config/apps/saved/views.py
+++ config/apps/saved/views.py
@@ -2,7 +2,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse
 from django.views.decorators.http import require_POST
 
 from apps.shop.models import Product, ProductImage
