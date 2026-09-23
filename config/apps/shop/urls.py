1d7556de3b8d9c73c4ea02347b37c8bcd26d7d5f
abolfazll mohammadi <156206718+AbolfazlMohammady@users.noreply.github.com>
2026-09-23 17:17:37 +0330

    Add product comment routes

--- config/apps/shop/urls.py
+++ config/apps/shop/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 
-from .views import CategoryDetailView, ProductDetailView, ShopIndexView
+from .views import CategoryDetailView, ProductCommentAddView, ProductCommentsView, ProductDetailView, ShopIndexView
 
 
 app_name = "shop"
@@ -9,4 +9,6 @@ urlpatterns = [
     path("", ShopIndexView.as_view(), name="index"),
     re_path(r"^category/(?P<slug>[^/]+)/$", CategoryDetailView.as_view(), name="category"),
     re_path(r"^product/(?P<slug>[^/]+)/$", ProductDetailView.as_view(), name="product"),
+    re_path(r"^product/(?P<slug>[^/]+)/comments/$", ProductCommentsView.as_view(), name="product_comments"),
+    re_path(r"^product/(?P<slug>[^/]+)/comments/add/$", ProductCommentAddView.as_view(), name="product_comment_add"),
 ]
