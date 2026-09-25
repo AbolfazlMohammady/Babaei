f67e457dc177f2701a8380cee0ea2ff94ae0fc87
abolfazll mohammadi <156206718+AbolfazlMohammady@users.noreply.github.com>
2026-09-25 08:06:38 +0330

    Add shop search route

--- config/apps/shop/urls.py
+++ config/apps/shop/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, re_path
 
-from .views import CategoryDetailView, ProductCommentAddView, ProductCommentsView, ProductDetailView, ShopIndexView
+from .views import CategoryDetailView, ProductCommentAddView, ProductCommentsView, ProductDetailView, ShopIndexView, ShopSearchView
 
 
 app_name = "shop"
 
 urlpatterns = [
     path("", ShopIndexView.as_view(), name="index"),
+    path("search/", ShopSearchView.as_view(), name="search"),
     re_path(r"^category/(?P<slug>[^/]+)/$", CategoryDetailView.as_view(), name="category"),
     re_path(r"^product/(?P<slug>[^/]+)/$", ProductDetailView.as_view(), name="product"),
     re_path(r"^product/(?P<slug>[^/]+)/comments/$", ProductCommentsView.as_view(), name="product_comments"),
